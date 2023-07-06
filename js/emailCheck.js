cb722d7114417015bd181f2dbcebc455ca6b0c8d
NitheeshKumar2811 <nitheeshkumarmurugesan281199@gmail.com>
2023-07-06 18:25:11 +0530

    contact page and email input changes done by Nitheesh

---
+++ js/emailCheck.js
@@ -0,0 +1,30 @@
+let form = document.getElementById('check');
+let mail = document.getElementById('mail');
+
+
+form.addEventListener('submit',function(event){
+    if(!validationInputs()){
+        event.preventDefault();
+    }
+});
+
+function validationInputs(){
+    let mailVal = mail.value;
+    let flag = true;
+
+     if(mailVal.trim()==''){
+         alert("Please enter an e-mail address.");
+         flag = false;
+     }
+    else if(!validation(mailVal)){
+        alert('Invalid e-mail address');
+        flag = false;
+    }
+    return flag;
+}
+
+function validation(value){
+    let validRegex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return  String(value).toLowerCase().match(validRegex);   
+}
+
